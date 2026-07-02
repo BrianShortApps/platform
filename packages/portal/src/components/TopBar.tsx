86e72f2185c83bfdff943e4e5bcdccdc80a06de3
IslandCoaster <short.brians@gmail.com>
2026-07-02 13:24:54 -0400

    PF-006: upgrade portal top bar

--- packages/portal/src/components/TopBar.tsx
+++ packages/portal/src/components/TopBar.tsx
@@ -5,10 +5,22 @@ export const TopBar = () => {
 
   return (
     <header className="portal-topbar">
-      <div className="portal-topbar__brand">{appName}</div>
+      <div className="portal-topbar__identity">
+        <div className="portal-topbar__mark">BSA</div>
+        <div>
+          <div className="portal-topbar__brand">{appName}</div>
+          <div className="portal-topbar__subtitle">Portal Foundation</div>
+        </div>
+      </div>
 
-      <div className="portal-topbar__user">
-        {currentUser?.name ?? "Unknown User"}
+      <div className="portal-topbar__actions">
+        <div className="portal-topbar__search">Search coming soon</div>
+        <button className="portal-topbar__button" type="button">
+          Alerts
+        </button>
+        <div className="portal-topbar__user">
+          {currentUser?.name ?? "Unknown User"}
+        </div>
       </div>
     </header>
   );
